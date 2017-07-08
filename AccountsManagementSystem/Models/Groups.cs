258b85b988cfb6d6f1e605e39bca2d10e1dc7afc
Shimul <shimul39ju@gmail.com>
2017-07-08 20:12:58 +0600

    Added Models

---
+++ AccountsManagementSystem/Models/Groups.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsManagementSystem.Models
+{
+    class Groups
+    {
+        public int GId { get; set; }
+        public string GroupName { get; set; }
+        public string Stype { get; set; }
+
+    }
+}
---
+++ AccountsManagementSystem/Models/SubAccountTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsManagementSystem.Models
+{
+    class SubAccountTypes
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public string AccountType { get; set; }
+
+    }
+}
