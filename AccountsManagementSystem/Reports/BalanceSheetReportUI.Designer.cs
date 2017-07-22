91d614205ffff094f9e324ef67bb1631bfb8aa46
Ikramunnesa <ikraafi@gmail.com>
2017-07-22 14:41:20 +0600

    balance sheet report ui added

---
+++ AccountsManagementSystem/Reports/BalanceSheetReportUI.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AccountsManagementSystem.Reports
+{
+    partial class BalanceSheetReportUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BSIdComboBox = new System.Windows.Forms.ComboBox();
+            this.GetButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.BSIdComboBox);
+            this.groupBox1.Controls.Add(this.GetButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(491, 281);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "BS ID                :";
+            // 
+            // BSIdComboBox
+            // 
+            this.BSIdComboBox.FormattingEnabled = true;
+            this.BSIdComboBox.Location = new System.Drawing.Point(200, 44);
+            this.BSIdComboBox.Name = "BSIdComboBox";
+            this.BSIdComboBox.Size = new System.Drawing.Size(172, 21);
+            this.BSIdComboBox.TabIndex = 4;
+            // 
+            // GetButton
+            // 
+            this.GetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GetButton.Location = new System.Drawing.Point(240, 201);
+            this.GetButton.Name = "GetButton";
+            this.GetButton.Size = new System.Drawing.Size(91, 35);
+            this.GetButton.TabIndex = 3;
+            this.GetButton.Text = "GET";
+            this.GetButton.UseVisualStyleBackColor = true;
+            this.GetButton.Click += new System.EventHandler(this.GetButton_Click);
+            // 
+            // BalanceSheetReportUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 316);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BalanceSheetReportUI";
+            this.Text = "BalanceSheetReportUI";
+            this.Load += new System.EventHandler(this.BalanceSheetReportUI_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox BSIdComboBox;
+        private System.Windows.Forms.Button GetButton;
+
+    }
+}
---
+++ AccountsManagementSystem/Reports/BalanceSheetReportUI.cs
@@ -0,0 +1,108 @@
+﻿using AccountsManagementSystem.DbGateway;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountsManagementSystem.Reports
+{
+    public partial class BalanceSheetReportUI : Form
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataReader rdr;
+        ConnectionString cs = new ConnectionString();
+        private int id;
+        public BalanceSheetReportUI()
+        {
+            InitializeComponent();
+        }
+
+        private void GetButton_Click(object sender, EventArgs e)
+        {
+            GetButton.Enabled = false;
+
+            ParameterField paramField1 = new ParameterField();
+
+
+            //creating an object of ParameterFields class
+            ParameterFields paramFields1 = new ParameterFields();
+
+            //creating an object of ParameterDiscreteValue class
+            ParameterDiscreteValue paramDiscreteValue1 = new ParameterDiscreteValue();
+
+            //set the parameter field name
+            paramField1.Name = "id";
+
+            //set the parameter value
+            paramDiscreteValue1.Value = BSIdComboBox.Text;
+
+            //add the parameter value in the ParameterField object
+            paramField1.CurrentValues.Add(paramDiscreteValue1);
+
+            //add the parameter in the ParameterFields object
+            paramFields1.Add(paramField1);
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "AccountDb";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            BSTotalX cr = new BSTotalX();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+
+            f2.crystalReportViewer1.ParameterFieldInfo = paramFields1;
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+            GetButton.Enabled = true;
+            BSIdComboBox.SelectedIndex = -1;
+        }
+
+        private void BalanceSheetReportUI_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                con = new SqlConnection(cs.DBConn);
+                con.Open();
+                string ct = "SELECT BId FROM BSEvent ORDER BY BId";
+                cmd = new SqlCommand(ct);
+                cmd.Connection = con;
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    BSIdComboBox.Items.Add(rdr[0]);
+                }
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
